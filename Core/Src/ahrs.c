1e228623ae8d77215883bce069ef22ad834fb561
zheyi613 <zheyi880613@gmail.com>
2023-06-02 15:22:05 +0800

    Complete the function of initial attitude

--- Core/Src/ahrs.c
+++ Core/Src/ahrs.c
@@ -67,13 +67,27 @@ static inline float dot_vec(float ax, float ay, float az,
         return dot;
 }
 
+/**
+ * @brief Cross two unit vector to get quaternion
+ * 
+ * @param ax
+ * @param ay 
+ * @param az 
+ * @param bx 
+ * @param by 
+ * @param bz 
+ * @param qa 
+ * @param qb 
+ * @param qc 
+ * @param qd 
+ */
 static void cross_vec2quat(float ax, float ay, float az,
                            float bx, float by, float bz,
                            float *qa, float *qb, float *qc, float *qd)
 {
         float cx, cy, cz;
-        float dot, cross_norm_square, cross_norm;
-        float half_angle, sin_val;
+        float dot, cross_norm_square;
+        float half_x, half_y, half_z;
 
         dot = dot_vec(ax, ay, az, bx, by, bz);
         cross_vec(ax, ay, az, bx, by, bz, &cx, &cy, &cz);
@@ -96,30 +110,35 @@ static void cross_vec2quat(float ax, float ay, float az,
                         *qd = 0.f;
                 }
                 *qa = 0.f;
-        } else if (cross_norm_square < 0.000001f) { /* angle ~ 0 */
-                *qa = 1.f;
-                *qb = 0.f;
-                *qc = 0.f;
-                *qd = 0.f;
         } else {
-                cross_norm = sqrtf(cross_norm_square);
-                half_angle = atan2f(cross_norm, dot) * 0.5f;
-                cx /= cross_norm;
-                cy /= cross_norm;
-                cz /= cross_norm;
-                *qa = cosf(half_angle);
-                sin_val = sinf(half_angle);
-                *qb = cx * sin_val;
-                *qc = cy * sin_val;
-                *qd = cz * sin_val;
+                half_x = ax + bx;
+                half_y = ay + by;
+                half_z = az + bz;
+                normalize_vec(&half_x, &half_y, &half_z);
+                *qa = dot_vec(ax, ay, az, half_x, half_y, half_z);
+                cross_vec(ax, ay, az, half_x, half_y, half_z, &cx, &cy, &cz);
+                *qb = cx;
+                *qc = cy;
+                *qd = cz;
         }
 }
 
+/**
+ * @brief Initialize quaternion of attitude with acceleration and
+ *        magnetic field
+ * 
+ * @param ax 
+ * @param ay 
+ * @param az 
+ * @param mx 
+ * @param my 
+ * @param mz 
+ */
 void ahrs_init(float ax, float ay, float az,
                float mx, float my, float mz)
 {
-        float wy_x, wy_y, wy_z;
         float y_x, y_y, y_z;
+        float by_x, by_y, by_z;
         float qz0, qz1, qz2, qz3;
         float qy0, qy1, qy2, qy3;
 #ifdef NED_FRAME
@@ -132,25 +151,33 @@ void ahrs_init(float ax, float ay, float az,
         normalize_vec(&mx, &my, &mz);
         /* get rotation of world z to body z */
         cross_vec2quat(ax, ay, az, 0, 0, 1, &qz0, &qz1, &qz2, &qz3);
-        /* rotate world y to make world z coincide with body z */
-        y_x = 2.f * (qz1 * qz2 - qz0 * qz3);
-        y_y = 1.f - 2.f * (qz1 * qz1 + qz3 * qz3);
-        y_z = 2.f * (qz2 * qz3 + qz0 * qz1);
+        /* rotate body y to make world z coincide with body z */
+        by_x = 2.f * (qz1 * qz2 + qz0 * qz3);
+        by_y = 2.f * (0.5f - qz1 * qz1 + qz3 * qz3);
+        by_z = 2.f * (qz2 * qz3 - qz0 * qz1);
         /* acceleration (wz) cross magnetic field to get world y */
-        cross_vec(ax, ay, az, mx, my, mz, &wy_x, &wy_y, &wy_z);
+        cross_vec(ax, ay, az, mx, my, mz, &y_x, &y_y, &y_z);
         normalize_vec(&y_x, &y_y, &y_z);
         /* compute quaternion between world y and body y */
         cross_vec2quat(y_x, y_y, y_z, by_x, by_y, by_z,
                        &qy0, &qy1, &qy2, &qy3);
         /* product two quaternion */
-        q0 = qy0 * qz0 - qy1 * qz1 - qy2 * qz2 - qy3 * qz3;
-        q1 = qy1 * qz0 + qy0 * qz1 - qy3 * qz2 + qy2 * qz3;
-        q2 = qy2 * qz0 + qy3 * qz1 + qy0 * qz2 - qy1 * qz3;
-        q3 = qy3 * qz0 - qy2 * qz1 + qy1 * qz2 + qy0 * qz3;
+        q0 = qz0 * qy0 - qz1 * qy1 - qz2 * qy2 - qz3 * qy3;
+        q1 = qz1 * qy0 + qz0 * qy1 - qz3 * qy2 + qz2 * qy3;
+        q2 = qz2 * qy0 + qz3 * qy1 + qz0 * qy2 - qz1 * qy3;
+        q3 = qz3 * qy0 - qz2 * qy1 + qz1 * qy2 + qz0 * qy3;
         /* normalize quaternion */
         normalize_quat(&q0, &q1, &q2, &q3);
 }
 
+/**
+ * @brief Initialize quaternion of attitude with acceleration
+ *        Assume yaw is zero.
+ * 
+ * @param ax 
+ * @param ay 
+ * @param az 
+ */
 void ahrs_init_imu(float ax, float ay, float az)
 {
 #ifdef NED_FRAME
@@ -161,6 +188,7 @@ void ahrs_init_imu(float ax, float ay, float az)
         normalize_vec(&ax, &ay, &az);
         /* get rotation of world z to body z */
         cross_vec2quat(ax, ay, az, 0, 0, 1, &q0, &q1, &q2, &q3);
+        normalize_quat(&q0, &q1, &q2, &q3);
 }
 
 /**
